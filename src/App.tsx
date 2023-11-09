b3d51a1666aaf27478f019b8a6bc0632b2f2156c
macbook kal <arnaud.pantenier@gmail.com>
2023-11-09 16:58:48 +0100

    ajout backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import { UnauthApp } from "./components/auth/UnAuthApp"
 /** UTILS */
 /** API */
 /** MUI */
-import { ThemeProvider } from "@mui/material"
+import { Backdrop, CircularProgress, ThemeProvider } from "@mui/material"
 import { theme } from "./theme/theme"
+
 /** AUTH */
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./firebase/firebase.config"
@@ -17,14 +18,17 @@ import { useEffect, useState } from "react"
 function App() {
 
   const [authUser, setAuthUser] = useState(false)
+  const [showBackdrop, setShowBackdrop] = useState(false)
 
   useEffect(() => {
+    setShowBackdrop(true)
     onAuthStateChanged(auth, user => {
       if (user) {
         setAuthUser(true)
       } else {
         setAuthUser(false)
       }
+      setShowBackdrop(false)
     })
   }, [])
 
@@ -32,7 +36,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       {
-        authUser ? (<AuthApp />) : (<UnauthApp />)
+        showBackdrop ? (
+        <div className="absolute top-0 bottom-0 w-full flex items-center justify-center">
+          <Backdrop open={true} sx={{bgcolor: '#111'}} />
+          <CircularProgress color="success" />
+        </div>) :  authUser ? (<AuthApp />) : (<UnauthApp />)
       }
     </ThemeProvider>
   )
