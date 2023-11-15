76ef4f4207b0a7c2f2ddfc293d8e3a86c465f87d
macbook kal <arnaud.pantenier@gmail.com>
2023-11-15 20:44:57 +0100

    Nettoyage console log

--- src/api/apiMovieDB.tsx
+++ src/api/apiMovieDB.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 export const clientAPI = async (endPoint: string) => {
     const page = 1
     const startChar = endPoint.includes('?') ? `&` : `?`
-    // endPoint = type = movie/5657484
     const keyLang = `${startChar}api_key=${API_KEY}&language=${lang}&page=${page}`
 
     return axios.get(`${BASE_URL}/${endPoint}${keyLang}`)
