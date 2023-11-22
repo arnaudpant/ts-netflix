1549e73a1e99ebc3f445248e71fffa5a9006c218
macbook kal <arnaud.pantenier@gmail.com>
2023-11-22 22:22:33 +0100

    changement bar env 2

--- src/api/apiMovieDB.tsx
+++ src/api/apiMovieDB.tsx
@@ -1,4 +1,4 @@
-import { API_KEY, BASE_URL, lang } from '../utils/config'
+import { API_KEY, lang } from '../utils/config'
 import axios from "axios";
 
 export const clientAPI = async (endPoint: string) => {
@@ -6,5 +6,5 @@ export const clientAPI = async (endPoint: string) => {
     const startChar = endPoint.includes('?') ? `&` : `?`
     const keyLang = `${startChar}api_key=${API_KEY}&language=${lang}&page=${page}`
 
-    return axios.get(`${BASE_URL}${endPoint}${keyLang}`)
+    return axios.get(`https://api.themoviedb.org/3/${endPoint}${keyLang}`)
 }
