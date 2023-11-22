8f23d347f661da715fecb2078a920465fb706915
macbook kal <arnaud.pantenier@gmail.com>
2023-11-22 21:33:45 +0100

    test https

--- src/api/apiMovieDB.tsx
+++ src/api/apiMovieDB.tsx
@@ -6,5 +6,5 @@ export const clientAPI = async (endPoint: string) => {
     const startChar = endPoint.includes('?') ? `&` : `?`
     const keyLang = `${startChar}api_key=${API_KEY}&language=${lang}&page=${page}`
 
-    return axios.get(`${BASE_URL}/${endPoint}${keyLang}`)
+    return axios.get(`https://api.themoviedb.org/3/${endPoint}${keyLang}`)
 }
