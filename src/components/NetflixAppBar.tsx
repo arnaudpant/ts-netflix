761ba3815bc8f1503e9db5ea26d2a6582406f0e1
macbook kal <arnaud.pantenier@gmail.com>
2023-11-25 14:32:58 +0100

    modif navbar + page search

--- src/components/NetflixAppBar.tsx
+++ src/components/NetflixAppBar.tsx
@@ -10,7 +10,7 @@ import { FaSearch } from "react-icons/fa";
 
 export const NetflixAppBar = () => {
 
-   // const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     const [appBarStyle, setAppBarStyle] = useState(
         {
@@ -61,12 +61,14 @@ export const NetflixAppBar = () => {
         <AppBarStyle>
             <Toolbar  >
                 <img src="/vignettes/netflix-logo.png" alt="logo Netflix" className="h-5" color="secondary" />
-                <Link to="/" className="ml-5 mr-2"><Typography variant="h6" className="title-header">Accueil</Typography></Link>
-                <Link to="/series" className="m-2"><Typography variant="h6" className="title-header">Séries</Typography></Link>
-                <Link to="/movies" className="m-2"><Typography variant="h6" className="title-header">Films</Typography></Link>
-                <Link to="/favoris" className="m-2"><Typography variant="h6" className="title-header">Ma liste</Typography></Link>
-                <FaSearch />
-                <div className="h-8 w-8 ml-auto cursor-pointer" onClick={logOut}>
+                <Link to="/" className="ml-5 mr-2 hover:scale-110"><Typography variant="h6" className="title-header">Accueil</Typography></Link>
+                <Link to="/series" className="m-2 hover:scale-110"><Typography variant="h6" className="title-header">Séries</Typography></Link>
+                <Link to="/movies" className="m-2 hover:scale-110"><Typography variant="h6" className="title-header">Films</Typography></Link>
+                <Link to="/favoris" className="m-2 hover:scale-110"><Typography variant="h6" className="title-header">Ma liste</Typography></Link>
+                <div className="ml-auto hover:scale-150">
+                    <Link to="/search"><FaSearch /></Link>
+                </div>
+                <div className="h-8 w-10 ml-10 cursor-pointer hover:scale-150" onClick={logOut}>
                     <img src="/avatar/avatar-default2.svg" alt="logo Netflix" />
                 </div>
             </Toolbar>
---
+++ src/pages/NetflixSearch.tsx
@@ -0,0 +1,11 @@
+
+
+const NetflixSearch = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NetflixSearch;
