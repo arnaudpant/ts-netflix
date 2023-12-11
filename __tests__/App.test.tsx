a4f6b26f3f45d82e4e546ceb4e486d1ff6396240
macbook kal <arnaud.pantenier@gmail.com>
2023-12-11 22:01:33 +0100

    modif tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -5,13 +5,13 @@ import { render, screen, waitForElementToBeRemoved } from './test-utils'
 import App from '../src/App'
 import React from 'react';
 
-test('Affichage du Loader', async () => {
+test('Affichage et suppression du Loader', async () => {
     render(<App />)
     expect(screen.getByRole('alert')).toBeInTheDocument()
     await waitForElementToBeRemoved(() => screen.getByRole('alert'))
     expect(screen.queryByRole('alert')).toBeNull()
 
-    screen.debug();
+    //screen.debug();
 })
 
 test('affichage titre accueil', async () => {
